3bf3b40d1f54ae5d06f18a774bb394e8f882817d
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2020-02-28 22:21:49 +0100

    Falling platform experiments

---
+++ Assets/FallingPlatform.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public float fallSpeed = 0.3f;
+
+    bool triggered = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider other = collision.collider;
+
+        if (other.gameObject.CompareTag("Lava"))
+        {
+            Debug.Log("HIT LAVA");
+
+            Destroy(gameObject);
+            return;
+        }
+
+        Debug.Log("HIT PLAYER");
+
+        triggered = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!triggered)
+        {
+            return;
+        }
+
+        transform.Translate(Vector3.down * fallSpeed);
+    }
+}

6393dae34b8dfcac7fc549f71bf7e67e47a9c9d2
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2020-02-28 21:18:35 +0100

    Fix credit rendering

---
+++ Assets/Credits.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+}

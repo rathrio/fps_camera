b9f851e6f30260a5eb43453231d01c5a2ede83d8
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2020-03-01 19:45:03 +0100

    Add simple level timer

---
+++ Assets/LevelTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LevelTimer : MonoBehaviour
+{
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = Time.timeSinceLevelLoad.ToString("F1") + "s";
+    }
+}

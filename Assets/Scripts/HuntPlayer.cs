eabd0b24f452c920282252098b82732cea099a9d
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2020-03-01 02:07:50 +0100

    Get rid of hunters in demo scenes

--- Assets/Scripts/HuntPlayer.cs
+++ Assets/Scripts/HuntPlayer.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class HuntPlayer : MonoBehaviour
 {
-    public float huntSpeed = 1f;
+    public float huntSpeed = 11f;
 
     Transform player;
     GameManager gameManager;
+    Renderer _renderer;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         gameManager = FindObjectOfType<GameManager>();
+        _renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, player.position, huntSpeed * Time.deltaTime);
+        // Hack to only hunt the player when they are not looking at this object.
+        // 
+        // Caveat: the object is considered visible to the renderer if it's shadow is visible as well.
+        if (_renderer.isVisible)
+        {
+            return;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, player.position, huntSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
